9f4bba3108915b0e9f1374ddb1cc885c3a3c92a5
Joost Baaij <joost@spacebabies.nl>
2017-08-13 12:37:38 +0100

    use AWS CLI to sync the buckets
    There is too much stuff in there that only becomes available from JavaScript. And it depends on the browser, OS, etc. Best to just sync the whole thing.

--- scrollytelling-export.bash
+++ scrollytelling-export.bash
@@ -17,8 +17,11 @@ then
   echo "USAGE: `basename $0` <URL to a single Scrollytelling>"
   echo "  e.g. `basename $0` https://app.scrollytelling.io/datwatjenietziet"
   echo
+  echo "The environment variable BUCKET needs to be set to the output bucket on S3."
   exit
 fi
+
+: "${BUCKET:?}"
 
 echo "# parse domain and path parts from the first command line argument"
 IFS=/; read -a urlparts <<<"${1#https://}"
@@ -69,6 +72,19 @@ _sed 's,https?:\/\/media\.scrollytelling\.io,\/media\.scrollytelling\.io,g' ${st
 echo "# media from the story CSS"
 grep -oiE "https?:\/\/media\.scrollytelling\.io.*?[^?\"']+" scrollytelling.link/entries/${story}.css | _xargs wget --force-directories --timestamping --no-verbose '{}'
 _sed 's,https?:\/\/media\.scrollytelling\.io,\/media\.scrollytelling\.io,g' scrollytelling.link/entries/${story}.css
+
+echo "# sync all the output directories using AWS CLI"
+cd output.scrollytelling.io
+types=(video_files audio_files)
+for type in "${types[@]}"
+do
+  folders=(v1/main/pageflow/$type/*/*/*)
+  for folder in "${folders[@]}"
+  do
+    aws s3 sync "s3://${BUCKET}/${folder}" "${folder}"
+  done
+done
+cd -
 
 # I don't know what idiot thought these files should be generated.
 find . -type f -name 'Icon?' -delete
