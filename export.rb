94f3a60a0b01fd3aa0d24cd5277255ddf02d60bd
Joost Baaij <joost@spacebabies.nl>
2018-10-25 18:58:56 +0200

    prepare to download files

--- export.rb
+++ export.rb
@@ -18,6 +18,13 @@ index = response.body.to_s
   .gsub('https://media.scrollytelling.com', '/media.scrollytelling.com')
   .gsub('https://output.scrollytelling.com', '/output.scrollytelling.com')
 
-filename = "#{uri.host}/#{uri.path}.html"
+slug = uri.path
+filename = "#{uri.host}/#{slug}.html"
 File.truncate(filename, 0)
 IO.write(filename, index)
+
+# download all files used in the story
+%w(video_files audio_files text_track_files image_files).each do |file_type|
+  response = HTTP.get("https://app.scrollytelling.io/editor/entries/#{slug}/files/#{file_type}.json")
+  IO.write("#{uri.host}/#{file_type}.json", response.body.to_s) if response.code == 200
+end
