dfb22b15fe572acaba043ffcabe326f6c40aa4a6
Joost Baaij <joost@spacebabies.nl>
2018-12-28 16:33:43 +0100

    bulk-update more files

--- step_3_account_index/deploy.sh
+++ step_3_account_index/deploy.sh
@@ -7,13 +7,15 @@ sed -ni '/csrf/!p' ../entries/*/*.html # strip CSRF tokens
 sed -ni '/PAGEFLOW_EDITOR/!p' ../entries/*/*.html # strip editor JS
 sed -i 's/ data-turbolinks-track="true"//g' ../entries/*/*.html # no turbolinks either
 
-sed -i 's\/media\media\g' ../entries/**/*.css
 sed -i 's\http://media\media\g' ../entries/**/*.html
 sed -i 's\https://media\media\g' ../entries/**/*.html
 sed -i 's\http://output\output\g' ../entries/**/*.html
 sed -i 's\https://output\output\g' ../entries/**/*.html
 sed -i 's\https://scrollytelling.link\scrollytelling.link\g' ../entries/**/*.html
 sed -i 's\../scrollytelling.link\scrollytelling.link\g' ../entries/**/*.html
+sed -i 's\/media\media\g' ../entries/**/*.css
+sed -i 's\/media\media\g' ../entries/**/*.js
+sed -i 's\/media\media\g' ../entries/**/*.html
 
 webpack
 
